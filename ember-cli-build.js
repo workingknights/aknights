eaca2d3510eb510308cf704dac64ce3bacd53900
workingknights <anthony.knights@gmail.com>
2017-02-03 14:38:25 +0900

    Added boostrap support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,10 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+	app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+  app.import('bower_components/jquery/dist/jquery.slim.min.js');
+  app.import('bower_components/tether/dist/js/tether.min.js');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+
   return app.toTree();
 };
