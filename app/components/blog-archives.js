b6a6fcf3ab5c92d363322f4d67f69d65dcf89725
workingknights <anthony.knights@gmail.com>
2017-02-06 23:25:10 +0900

    refactored blog code into components

--- app/components/blog-archives.js
+++ app/components/blog-archives.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.Component.extend({
 
 	monthGroups: Ember.computed('model', function() {
 		var monthGroups = new Ember.A();
 		var entries = this.model;
 
-		entries.forEach(function(entry){
+		entries.forEach(function(entry) {
 			var month = entry.get('month');
 			var year = entry.get('year');
 			var monthGroup = monthGroups.findBy('month', month);
@@ -14,7 +14,11 @@ export default Ember.Controller.extend({
 			if (Ember.isPresent(monthGroup)) {
 				Ember.get(monthGroup, 'entries').push(entry);
 			} else {
-				monthGroup = { month: month, year: year, entries: [entry] };
+				monthGroup = {
+					month: month,
+					year: year,
+					entries: [entry]
+				};
 				monthGroups.push(monthGroup);
 			}
 		});
